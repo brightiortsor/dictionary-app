f20e2cbccee6f365ee49c52b90195338b45f8a47
Bright-Iortsor <iortsor@gmail.com>
2022-10-18 06:52:59 +0100

    ui changes

---
+++ app.js
@@ -0,0 +1,28 @@
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+const result = document.getElementById("result");
+const sound = document.getElementById("sound");
+const btn = document.getElementById("search-btn");
+
+btn.addEventListener("click", () => {
+  let searchWord = document.getElementById("search-word").value;
+  fetch(`${url}${searchWord}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      result.innerHTML = `
+<div class="word">
+          <h3>${searchWord}</h3>
+          <button><i class="fa-solid fa-volume-high"></i></button>
+        </div>
+
+        <div class="details">
+          <p>${data[0].meanings[0].partOfSpeech}</p>
+          <p class="phonie">${data[0].phonetics[1].text}</p>
+        </div>
+        <p class="meaning">${data[0].meanings[1].definitions[0].definition}</p>
+        <p class="word-examp">${
+          data[0].meanings[0].definitions[0].example || ""
+        }</p>
+`;
+    });
+});
